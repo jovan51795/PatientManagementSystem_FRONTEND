c00490a22322ac1bfbc2eac388882771bacb4ae6
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-11-09 20:17:09 +0800

    add patient page

--- src/app/demo/components/patients-form/patients-form.component.ts
+++ src/app/demo/components/patients-form/patients-form.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
+import { IDoctor } from 'src/app/interfaces/docker';
+import { DoctorService } from 'src/app/services/doctor/doctor.service';
 
 @Component({
-  selector: 'app-patients-form',
-  templateUrl: './patients-form.component.html',
-  styleUrls: ['./patients-form.component.scss']
+    selector: 'app-patients-form',
+    templateUrl: './patients-form.component.html',
+    styleUrls: ['./patients-form.component.scss'],
 })
-export class PatientsFormComponent {
+export class PatientsFormComponent implements OnInit {
+    gender: string = '';
+    status: string[] = [
+        'ACTIVE',
+        'DISCHARGED',
+        'ON_HOLD',
+        'DECEASED',
+        'SCHEDULED',
+        'NO_SHOW',
+        'CANCELLED',
+        'IN_TREATMENT',
+        'RECOVERED',
+        'CRITICAL',
+        'PENDING_ADMISSION',
+    ];
 
+    doctors: IDoctor[] = [];
+    public patientForm = this.fb.group({
+        first_name: ['', [Validators.required]],
+        middle_name: ['', [Validators.required]],
+        last_name: ['', [Validators.required]],
+        birthday: ['', [Validators.required]],
+        place_of_birth: ['', [Validators.required]],
+        gender: ['', [Validators.required]],
+        contact: ['', [Validators.required]],
+        emergency_contact: ['', [Validators.required]],
+        status: ['', [Validators.required]],
+        patientRecords: this.fb.array([
+            this.fb.group({
+                prescriptions: [''],
+                notes: [''],
+                diagnose: [''],
+                physician: [null],
+            }),
+        ]),
+    });
+    constructor(
+        private doctorService: DoctorService,
+        private fb: FormBuilder
+    ) {}
+
+    ngOnInit(): void {
+        this.getAllDoctor();
+    }
+
+    getAllDoctor() {
+        return this.doctorService.getAllDoctors().subscribe((x) => {
+            if (x.status === 1) {
+                this.doctors = x.data;
+            }
+        });
+    }
+
+    submit() {
+        const patientData = this.patientForm.getRawValue();
+        console.log(patientData);
+    }
+
+    get getPatientRecord(): FormArray {
+        return this.patientForm.get('patientRecords') as FormArray;
+    }
 }
--- src/app/demo/components/patients-form/patients-form.module.ts
+++ src/app/demo/components/patients-form/patients-form.module.ts
@@ -2,9 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PatientsFormComponent } from './patients-form.component';
 import { PatientsFormRoutingModule } from './patients-form-routing.module';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { FormsModule } from '@angular/forms';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { ButtonModule } from 'primeng/button';
+import { SharedModule } from 'src/app/shared/shared/shared.module';
 
 @NgModule({
     declarations: [PatientsFormComponent],
-    imports: [CommonModule, PatientsFormRoutingModule],
+    imports: [
+        CommonModule,
+        PatientsFormRoutingModule,
+        InputTextModule,
+        RadioButtonModule,
+        FormsModule,
+        DropdownModule,
+        InputTextareaModule,
+        ButtonModule,
+        SharedModule,
+    ],
 })
 export class PatientsFormModule {}
