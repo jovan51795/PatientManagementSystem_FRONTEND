2603992d2c9afa058a12f15557b8bfbc9570f033
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-11-09 21:54:08 +0800

    display patients in the table

--- src/app/demo/components/patients/patients.component.ts
+++ src/app/demo/components/patients/patients.component.ts
@@ -3,44 +3,49 @@ import { MenuItem } from 'primeng/api';
 import { Customer, Representative } from 'src/app/demo/api/customer';
 import { CustomerService } from 'src/app/demo/service/customer.service';
 import { Table } from 'primeng/table';
+import { PatientService } from 'src/app/services/patients/patient.service';
+import { IPatient } from 'src/app/interfaces/patient';
 
 @Component({
-  selector: 'app-patients',
-  templateUrl: './patients.component.html',
-  styleUrls: ['./patients.component.scss']
+    selector: 'app-patients',
+    templateUrl: './patients.component.html',
+    styleUrls: ['./patients.component.scss'],
 })
-export class PatientsComponent implements OnInit{
-
+export class PatientsComponent implements OnInit {
     items!: MenuItem[];
-    customers1: Customer[] = [];
     loading: boolean = true;
+    patients: IPatient[] = [];
     @ViewChild('filter') filter!: ElementRef;
 
-    constructor(
-        private customerService: CustomerService,
-    ){}
+    constructor(private patientService: PatientService) {}
 
     ngOnInit(): void {
         this.items = [
             { label: 'Add New', icon: 'pi pi-fw pi-plus' },
-            { label: 'Remove', icon: 'pi pi-fw pi-minus' }
+            { label: 'Remove', icon: 'pi pi-fw pi-minus' },
         ];
-        this.customerService.getCustomersLarge().then(customers => {
-            this.customers1 = customers;
-            this.loading = false;
 
-            // @ts-ignore
-            this.customers1.forEach(customer => customer.date = new Date(customer.date));
+        this.getAllPatients();
+    }
+
+    getAllPatients() {
+        this.patientService.getAllPatients().subscribe((x) => {
+            if (x.status === 1) {
+                this.loading = false;
+                this.patients = x.data;
+            }
         });
     }
 
     onGlobalFilter(table: Table, event: Event) {
-        table.filterGlobal((event.target as HTMLInputElement).value, 'contains');
+        table.filterGlobal(
+            (event.target as HTMLInputElement).value,
+            'contains'
+        );
     }
 
     clear(table: Table) {
         table.clear();
         this.filter.nativeElement.value = '';
     }
-
 }
