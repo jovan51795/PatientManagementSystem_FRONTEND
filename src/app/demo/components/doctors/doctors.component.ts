3813c2426160bab72c82525e759581db21386663
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-11-09 10:35:06 +0800

    add table

--- src/app/demo/components/doctors/doctors.component.ts
+++ src/app/demo/components/doctors/doctors.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { Customer, Representative } from 'src/app/demo/api/customer';
+import { CustomerService } from 'src/app/demo/service/customer.service';
+import { Table } from 'primeng/table';
 
 @Component({
-  selector: 'app-doctors',
-  templateUrl: './doctors.component.html',
-  styleUrls: ['./doctors.component.scss']
+    selector: 'app-doctors',
+    templateUrl: './doctors.component.html',
+    styleUrls: ['./doctors.component.scss'],
 })
 export class DoctorsComponent {
+    items!: MenuItem[];
+    customers1: Customer[] = [];
+    loading: boolean = true;
+    @ViewChild('filter') filter!: ElementRef;
 
+    constructor(private customerService: CustomerService) {}
+
+    ngOnInit(): void {
+        this.items = [
+            { label: 'Add New', icon: 'pi pi-fw pi-plus' },
+            { label: 'Remove', icon: 'pi pi-fw pi-minus' },
+        ];
+        this.customerService.getCustomersLarge().then((customers) => {
+            this.customers1 = customers;
+            this.loading = false;
+
+            // @ts-ignore
+            this.customers1.forEach(
+                (customer) =>
+                    (customer.date = new Date(customer.date).toDateString())
+            );
+        });
+    }
+
+    onGlobalFilter(table: Table, event: Event) {
+        table.filterGlobal(
+            (event.target as HTMLInputElement).value,
+            'contains'
+        );
+    }
+
+    clear(table: Table) {
+        table.clear();
+        this.filter.nativeElement.value = '';
+    }
 }
--- src/app/demo/components/doctors/doctors.module.ts
+++ src/app/demo/components/doctors/doctors.module.ts
@@ -2,9 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DoctorsRoutingModule } from './doctors-routing.module';
 import { DoctorsComponent } from './doctors.component';
+import { StyleClassModule } from 'primeng/styleclass';
+import { TableModule } from 'primeng/table';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { MenuModule } from 'primeng/menu';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
     declarations: [DoctorsComponent],
-    imports: [CommonModule, DoctorsRoutingModule],
+    imports: [
+        CommonModule,
+        DoctorsRoutingModule,
+        StyleClassModule,
+        TableModule,
+        PanelMenuModule,
+        MenuModule,
+        InputTextModule,
+        ButtonModule,
+    ],
 })
 export class DoctorsModule {}
