38a4248bda795ce967a4aaa9f08a2c29c576235f
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2023-11-11 19:53:11 +0800

    fix

--- src/app/interceptors/http-error.interceptor.ts
+++ src/app/interceptors/http-error.interceptor.ts
@@ -24,9 +24,8 @@ export class HttpErrorInterceptor implements HttpInterceptor {
                 console.log('the error', error);
                 if (error.status === 403 || error.status === 401) {
                     sessionStorage.removeItem('pms-user');
+                    window.location.reload();
                     this.router.navigate(['/auth/login']);
-                    //window.location.href = '/#/auth/login';
-                    // Redirect to the login page or perform other actions as needed
                 } else {
                     alert('Unknown error occurred');
                     console.error(error);
