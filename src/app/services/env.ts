3d5e8adcd436b77e1d907962d3fd31b8cc9e7a40
jovan51795 <62101107+jovan51795@users.noreply.github.com>
2024-02-11 09:05:25 +0800

    minor fix

--- src/app/services/env.ts
+++ src/app/services/env.ts
@@ -1,6 +1,6 @@
 export const env = {
-    // BASE_URL : "http://localhost:7000/api/v1/auth",
-    // PRIVATE_URL : "http://localhost:7000/api/v1/private"
-    BASE_URL: '/api/auth',
-    PRIVATE_URL: '/api/v1/private',
+    // BASE_URL: 'http://localhost:8080/api/v1/auth',
+    // PRIVATE_URL: 'http://localhost:8080/api/v1/private',
+    BASE_URL: 'http://3.0.101.186:8080/api/v1/auth',
+    PRIVATE_URL: 'http://3.0.101.186:8080/api/v1/private',
 };
